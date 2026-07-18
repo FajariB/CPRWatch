4cc2ab4b44befcd74b5dcea28207191e9adfc7f3
fajari <fajaribagas09@gmail.com>
2026-07-18 16:38:40 +0700

    Initial Commit

---
+++ CPRWatch Watch App/CPRWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPRWatchApp.swift
+//  CPRWatch Watch App
+//
+//  Created by fajari bagas on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CPRWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CPRWatch/CPRWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CPRWatchApp.swift
+//  CPRWatch
+//
+//  Created by fajari bagas on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CPRWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
